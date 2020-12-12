56e408482f3e569220ca122f2a7935d20be94b07
GoldenDisc <69274109+GoldenDisc@users.noreply.github.com>
2020-12-12 15:09:33 -0700

    Update GmailFilter.py

--- GmailFilter.py
+++ GmailFilter.py
@@ -2,9 +2,9 @@ from __future__ import print_function
 import sys
 import schedule
 import time
-import datetime
 import pickle
 import os.path
+from datetime import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -20,38 +20,11 @@ star_list = ["<The adresses of the Emails you want to star>"]
 
 star_name = ["<The names of the Emails you want to star>"]
 
-spam_class = []
+spam_class = []     # Optional to use, every filtered Email has an object created which is placed in this list, used by certain given funcs
 
+time_format = "%H:%M:%S"
 
-# Connecting to Gmail servers, signing in
-
-SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
-
-creds = None
-
-if os.path.exists('token.pickle'):
-
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-
-
-if not creds or not creds.valid:
-
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-
-        creds = flow.run_local_server(port=0)
-
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
-
-service = build('gmail', 'v1', credentials=creds)
-
-with open("Spam.txt", "a") as spam_log:
-    spam_log.write(f"\n======== - {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]} - START OF LOG - ========\n\n")
+full_format = "%m/%d/%Y %H:%M:%S"
 
 
 # Defining key classes & functions
@@ -93,7 +66,7 @@ def nuke():
     messages = results.get('messages', [])
 
     with open("Spam.txt", "a") as spam_log:
-        spam_log.write(f"===== - TACTICAL NUKE, INCOMING! - {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]} - =====\n\n")
+        spam_log.write(f"===== - TACTICAL NUKE, INCOMING! - {datetime.now().strftime(full_format)}  - =====\n\n")
 
 
     if not messages:
@@ -102,6 +75,7 @@ def nuke():
     else:
 
         for message in messages:
+            
             service.users().messages().modify(userId="me", id=message["id"], body={"addLabelIds": ["TRASH"]}).execute()
 
             service.users().messages().modify(userId="me", id=message["id"], body={"removeLabelIds": ["UNREAD"]}).execute()
@@ -113,7 +87,7 @@ def filterfunc(num):
     messages = results.get('messages', [])
 
     with open("Spam.txt", "a") as spam_log:
-        spam_log.write(f"===== - Check {num} - {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]} - =====\n\n")
+        spam_log.write(f"===== - Check {num} - {datetime.now().strftime(time_format)} - =====\n\n")
 
     num += 1
 
@@ -122,6 +96,7 @@ def filterfunc(num):
 
     else:
         for message in messages:
+
             msg = service.users().messages().get(userId="me", id=message["id"]).execute()   
 
             email_data = msg["payload"]["headers"]
@@ -163,43 +138,77 @@ def filterfunc(num):
 
 
             if from_name[-1] in spam_list or from_name[0:-1] in spam_name:
+
                 service.users().messages().modify(userId="me", id=message["id"], body={"removeLabelIds": ["INBOX"]}).execute()
 
                 service.users().messages().modify(userId="me", id=message["id"], body={"removeLabelIds": ["UNREAD"]}).execute()
 
                 spam_class.append(Email(message_data))
 
+
                 with open("Spam.txt", "a") as spam_log:
 
                     try:
-                        spam_log.write(f"Filtered at {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]}: From {message_data['name']} with the address {message_data['address']}, {message_data['subject']}\n\n")
+                        spam_log.write(f"Filtered at {datetime.now().strftime(time_format)}: From {message_data['name']} with the address {message_data['address']}, {message_data['subject']}\n\n")
 
                     except UnicodeEncodeError:
-                        spam_log.write(f"Filtered, {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]}: CRITICAL ENCODING ERROR!\n\n")
+                        spam_log.write(f"Filtered, {datetime.now().strftime(time_format)}: CRITICAL ENCODING ERROR!\n\n")
 
 
             elif from_name[-1] in star_list:
                 service.users().messages().modify(userId="me", id=message["id"], body={"addLabelIds": ["STARRED"]}).execute()
 
+
                 with open("Spam.txt", "a") as spam_log:
 
                     try:
-                        spam_log.write(f"Starred at {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]}: From {message_data['name']} with the address {message_data['address']}, {message_data['subject']}\n\n")
+                        spam_log.write(f"Starred at {datetime.now().strftime(time_format)}: From {message_data['name']} with the address {message_data['address']}, {message_data['subject']}\n\n")
 
                     except UnicodeEncodeError:
-                        spam_log.write(f"Starred, {(str(datetime.datetime.now()).split('.')[0]).split(' ')[1]}: CRITICAL ENCODING ERROR!\n\n")
+                        spam_log.write(f"Starred, {datetime.now().strftime(time_format)}: CRITICAL ENCODING ERROR!\n\n")
 
 
     return num
 
 
-# Execution of the key function(s)
-
-num = 1
+# Connecting to Gmail servers, signing in
 
 if __name__ == '__main__':
 
-   for numb in range(0, 10**10):
+    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+    creds = None
+
+    if os.path.exists('token.pickle'):
+
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    with open("Spam.txt", "a") as spam_log:
+        spam_log.write(f"\n======== - {datetime.now().strftime(full_format)} - START OF LOG - ========\n\n")
+        
+
+# Execution of the key function(s)
+
+    num = 1
+
+    for numb in range(0, 10**10):
 
         num = filterfunc(num)
 
